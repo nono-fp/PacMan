15c31244cba99d790f5a7cf653f72d8ce0cc78b7
Noemie <154456092+nono-fp@users.noreply.github.com>
2025-04-01 15:41:26 +0200

    Update pacman.py

--- pacman.py
+++ pacman.py
@@ -9,17 +9,35 @@ class Pacman(object):
     
     def __init__(self):
         self.name = PACMAN
-        self.position = Vecteur2D(LARGEUR_FENETRE // 2, HAUTEUR_FENETRE // 2)
+        #self.position = Vecteur2D(LARGEUR_FENETRE // 2, HAUTEUR_FENETRE // 2)
         self.directions = {STOP:Vecteur2D(),UP:Vecteur2D(0,-1), DOWN:Vecteur2D(0,1), RIGHT:Vecteur2D(1,0), LEFT : Vecteur2D(-1,0)}
         self.direction = STOP
         self.speed = 100 * LARGEUR_DE_TUILE /20
         self.radius = 10
         self.color = YELLOW
+        self.node = node
+        self.setPosition()
+        
+    def setPosition(self):
+        self.position = self.node.position.copy()
         
     def update(self,dt):
-        self.position += self.directions[self.direction]*self.speed*dt
+        #self.position += self.directions[self.direction]*self.speed*dt
         direction = self.getValidKey()
         self.direction = direction
+        self.node = self.getNewTarget(direction)
+        self.setPosition()
+        
+    def validDirection(self, direction):
+        if direction is not STOP:
+            if self.node.voisins[direction] is not None:
+                return True
+        return False
+
+    def getNewTarget(self, direction):
+        if self.validDirection(direction):
+            return self.node.voisins[direction]
+        return self.node
         
     def getValidKey(self):
         key_pressed = py.key.get_pressed()
