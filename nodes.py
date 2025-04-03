42d77c5d0ab3fbb1999c2f29f96e7466c2bc52eb
Noemie <154456092+nono-fp@users.noreply.github.com>
2025-04-03 16:56:41 +0200

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -1,30 +1,26 @@
-# -*- coding: utf-8 -*-
-#neighbor : voisins
-#nodeList : nodeListe
-import pygame as py
+import pygame
+from vector import Vector2
 from constants import *
-from Vecteurs import Vecteur2D
 
 class Node(object):
-    
-    def __init__(self,x,y):
-        self.position = Vecteur2D(x,y)
-        self.voisins = {UP : None, DOWN : None, LEFT : None, RIGHT : None}
-        
-    def render(self,screen):
-        for n in self.voisins.keys():
-            if self.voisins[n] is not None:
-                ligne_start = self.position.convertir_tuple()
-                ligne_end = self.voisins[n].position.convertir_tuple()
-                py.draw.line(screen,WHITE,ligne_start,ligne_end,4)
-                py.draw.circle(screen,RED,self.position.asInt(),12)
-                
-                
+    def __init__(self, x, y):
+        self.position = Vector2(x, y)
+        self.neighbors = {UP:None, DOWN:None, LEFT:None, RIGHT:None}
+
+    def render(self, screen):
+        for n in self.neighbors.keys():
+            if self.neighbors[n] is not None:
+                line_start = self.position.asTuple()
+                line_end = self.neighbors[n].position.asTuple()
+                pygame.draw.line(screen, WHITE, line_start, line_end, 4)
+                pygame.draw.circle(screen, RED, self.position.asInt(), 12)
+
+
+
 class NodeGroup(object):
-    
     def __init__(self):
-        self.nodeListe =[]
-        
+        self.nodeList = []
+
     def setupTestNodes(self):
         nodeA = Node(80 ,80)
         nodeB = Node(160, 80)
@@ -33,24 +29,24 @@ class NodeGroup(object):
         nodeE = Node(208, 160)
         nodeF = Node(80, 320)
         nodeG = Node(208, 320)
-        nodeA.voisins[RIGHT] = nodeB
-        nodeA.voisins[DOWN] = nodeC
-        nodeB.voisins[LEFT] = nodeA
-        nodeB.voisins[DOWN] = nodeD
-        nodeC.voisins[UP] = nodeA
-        nodeC.voisins[RIGHT] = nodeD
-        nodeC.voisins[DOWN] = nodeF
-        nodeD.voisins[UP] = nodeB
-        nodeD.voisins[LEFT] = nodeC
-        nodeD.voisins[RIGHT] = nodeE
-        nodeE.voisins[LEFT] = nodeD
-        nodeE.voisins[DOWN] = nodeG
-        nodeF.voisins[UP] = nodeC
-        nodeF.voisins[RIGHT] = nodeG
-        nodeG.voisins[UP] = nodeE
-        nodeG.voisins[LEFT] = nodeF
-        self.nodeListe = [nodeA, nodeB, nodeC, nodeD, nodeE, nodeF, nodeG]
+        nodeA.neighbors[RIGHT] = nodeB
+        nodeA.neighbors[DOWN] = nodeC
+        nodeB.neighbors[LEFT] = nodeA
+        nodeB.neighbors[DOWN] = nodeD
+        nodeC.neighbors[UP] = nodeA
+        nodeC.neighbors[RIGHT] = nodeD
+        nodeC.neighbors[DOWN] = nodeF
+        nodeD.neighbors[UP] = nodeB
+        nodeD.neighbors[LEFT] = nodeC
+        nodeD.neighbors[RIGHT] = nodeE
+        nodeE.neighbors[LEFT] = nodeD
+        nodeE.neighbors[DOWN] = nodeG
+        nodeF.neighbors[UP] = nodeC
+        nodeF.neighbors[RIGHT] = nodeG
+        nodeG.neighbors[UP] = nodeE
+        nodeG.neighbors[LEFT] = nodeF
+        self.nodeList = [nodeA, nodeB, nodeC, nodeD, nodeE, nodeF, nodeG]
 
     def render(self, screen):
-        for node in self.nodeListe:
+        for node in self.nodeList:
             node.render(screen)
