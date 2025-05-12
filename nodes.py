d00364ef8bed3c7b6fbce4e8eb58e70915bd0758
Noemie <154456092+nono-fp@users.noreply.github.com>
2025-05-12 10:24:10 +0200

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -8,7 +8,7 @@ import numpy as np
 class Node(object):
     def __init__(self, x, y):
         self.position = Vector2(x, y)
-        self.neighbors = {UP:None, DOWN:None, LEFT:None, RIGHT:None}
+        self.neighbors = {UP:None, DOWN:None, LEFT:None, RIGHT:None, PORTAL : None}
 
     def render(self, screen):
         for n in self.neighbors.keys():
@@ -24,8 +24,8 @@ class NodeGroup(object):
     def __init__(self,level):
         self.level = level
         self.nodesLUT = {}
-        self.nodeSymbols = ['+']
-        self.pathSymbols = ['.']
+        self.nodeSymbols = ['+', 'P', 'n']
+        self.pathSymbols = ['.', '-', '|', 'p']
         data = self.readMazeFile(level)
         self.createNodeTable(data)
         self.connectHorizontally(data)
@@ -92,6 +92,13 @@ class NodeGroup(object):
     def getStartTempNode(self):
         nodes = list(self.nodesLUT.values())
         return nodes[0]
+    
+    def setPortalPair(self, pair1, pair2):
+        key1 = self.constructKey (*pair1)
+        key2 = self.constructKey(*pair2)
+        if key1 in self.nodesLUT.keys() and key2 in self.nodesLUT.keys():
+            self.nodesLUT[key1].neighbors[PORTAL] = self.nodesLUT[key2]
+            self.nodesLUT[key2].neighbors[PORTAL] = self.nodesLUT[key1]
 
     def render(self, screen):
         for node in self.nodesLUT.values():
