2ae035148463df8c28a36bf5b7143184f1f3cd38
Noemie <154456092+nono-fp@users.noreply.github.com>
2025-05-13 16:10:15 +0200

    Add files via upload

---
+++ ghosts.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import pygame
+from pygame.locals import *
+from vector import Vector2
+from constants import*
+from entity import Entity
+
+
+
+class Ghost(Entity):
+    def __init__(self, node):
+        Entity.__init__(self, node)
+        self.name = GHOST
+        self.points = 200
+        self.goal = Vector2()
+        self.directionMethod = self.goalDirection
