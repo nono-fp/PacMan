ad72d87d1da865ed044d5c19af95ea390dc57333
Noemie <154456092+nono-fp@users.noreply.github.com>
2025-05-22 16:57:04 +0200

    Update constants.py

--- constants.py
+++ constants.py
@@ -8,24 +8,41 @@ NROWS = 30 # NROWS
 SCREENWIDTH =  NCOLS * TILEWIDTH
 SCREENHEIGHT = NROWS * TILEHEIGHT
 SCREENSIZE = (SCREENWIDTH, SCREENHEIGHT)
+
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 YELLOW = (255,255,0)
 RED = (255, 0,0)
+PINK = (255, 100, 150)
+TEAL = (100, 255, 255)
+ORANGE = (230, 190, 40)
+
 STOP = 0
 UP = 1
-DOWN = 2
-LEFT = 3
-RIGHT = 4
+DOWN = -1
+LEFT = 2
+RIGHT = -2
+
 PORTAL = 3 #nbr aléatoire pas d'importance
 PACMAN = 0
 PELLET  = 1
 POWERPELLET = 2
 GHOST = 3
+
+
 SCATTER = 0 #dispersion
 CHASE = 1 #poursuite
 FREIGHT = 2 #fret
 SPAWN = 3 # apparition
+
+
+#nom des fantômes
+BLINKY = 4
+PINKY = 5
+INKY = 6
+CLYDE = 7
+
+
 """"
 
 X" : espace vide
